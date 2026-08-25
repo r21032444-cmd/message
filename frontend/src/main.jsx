7b2402fedb2c5b9b8e8a6e57af748e98d2205bc5
run228 <run228@users.noreply.github.com>
2026-08-25 17:25:30 +0300

    Add logging to diagnose blank screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+console.log('main.jsx: Starting app...')
+const root = document.getElementById('root')
+console.log('main.jsx: root element:', root)
+
+if (!root) {
+  console.error('Root element not found!')
+  document.body.innerHTML = '<h1>Root element not found!</h1>'
+} else {
+  console.log('main.jsx: Creating root')
+  createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+  console.log('main.jsx: App rendered')
+}
